6d7f9f6d5c2ff34437b1d7574622a7a36b75fbbe
ProjectBlackSpider <lebogangshokane@gmail.com>
2023-03-12 17:31:48 +0200

    Add files via upload
    codeComprehension

---
+++ question_1.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        int sum=0;
+
+       // String[] arrItems = scanner.nextLine().split(" ");
+        //scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+            sum+=arr[i];
+            //int arrItem = Integer.parseInt(arrItems[i]);
+            //arr[i] = arrItem;
+        }
+        Arrays.sort(arr);
+        int min=arr[4];
+        int max=arr[0];
+        int minSum = sum-min;
+        int maxSum = sum-max;
+        System.out.println(minSum+" "+maxSum);
+
+       // miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
